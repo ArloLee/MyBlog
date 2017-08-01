686048db65394621417e372fdf8deab78af238e3
lee <admin@leebor.cn>
2017-08-01 18:40:47 +0800

    nginx+uwsgi+supervisor configure

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
 echo ">>>>>>start MyBlog web"
-source $PATH
-python manage.py runserver 0.0.0.0:5210
+
+exec python manage.py runserver '0.0.0.0:5210' >> /var/log/MyBlog.log 2>&1 
+
+if [ $? -eq 0 ];then
+	echo ">>>>>> Start success on 0.0.0.0:5210"
+else
+	echo ">>>>>> Start Faild +.+||"
+fi
